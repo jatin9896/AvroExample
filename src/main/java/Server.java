1f263858ec7b05511043ec5521c9f5894cfb4e1d
jatin <jatin.demla@knoldus.in>
2017-05-08 16:51:30 +0530

    example completed

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,11 +1,24 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DecoderFactory;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-
+/*
+* Listen to client and deserialize the recieved bytes
+*/
 public class Server extends Thread {
+    public static void main(String args[]) {
+        Thread t = new Server();
+        t.start();
+    }
     public void run() {
         ServerSocket serverSocket = null;
         try {
@@ -13,34 +26,28 @@ public class Server extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        while(true) {
+        while (true) {
             try {
-                System.out.println("Waiting for client on port " +
-                        serverSocket.getLocalPort() + "...");
+                System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
                 Socket server = serverSocket.accept();
-
                 System.out.println("Just connected to " + server.getRemoteSocketAddress());
-                DataInputStream in = new DataInputStream(server.getInputStream());
-
-                System.out.println(in.read());
-                DataOutputStream out = new DataOutputStream(server.getOutputStream());
-                out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
-                        + "\nGoodbye!");
+                Schema schema = new Schema.Parser().parse(new File("src/employee.avsc"));
+                DatumReader<GenericRecord> dr = new GenericDatumReader<GenericRecord>(schema);
+                GenericRecord emp = new GenericData.Record(schema);
+                InputStream in = server.getInputStream();
+                DecoderFactory decoderFactory = new DecoderFactory();
+                BinaryDecoder binaryDecoder = decoderFactory.binaryDecoder(in, null);
+                dr.read(emp, binaryDecoder);
+                System.out.println("Data get from avro" + emp);
                 server.close();
 
-            }catch(SocketTimeoutException s) {
+            } catch (SocketTimeoutException s) {
                 System.out.println("Socket timed out!");
                 break;
-            }catch(IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
                 break;
             }
         }
-    }
-    public static void main(String args[]) {
-
-            Thread t = new Server();
-            t.start();
-
     }
 }
